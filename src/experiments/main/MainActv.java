f057d4922d7d3ca3045f56f0285b3ae8503aba5e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-09-20 15:12:18 +0900

    B1
    v-1.1
    ---
    1. List => Shown

--- src/experiments/main/MainActv.java
+++ src/experiments/main/MainActv.java
@@ -1,19 +1,53 @@
 package experiments.main;
 
+import java.util.ArrayList;
+
+import experiments.utils.CONS.Admin;
+import experiments.utils.CONS;
 import experiments.utils.Methods;
 import android.os.Bundle;
-import android.app.Activity;
+import android.os.Vibrator;
+import android.app.ListActivity;
+import android.content.Context;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActv extends Activity {
+public class MainActv extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.actv_main);
+		
+		this.setTitle(this.getClass().getName());
+		
+		Admin.vib = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
 	}
 
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		
+		/*********************************
+		 * Prep: ListView
+		 *********************************/
+		// List items
+		CONS.Admin.list_MainActv = new ArrayList<String>();
+		
+		CONS.Admin.list_MainActv.add(this.getString(R.string.mainactv_list_position));
+		
+		CONS.Main.mainAdapter = new ArrayAdapter<String>(
+									this, 
+									android.R.layout.simple_list_item_1, 
+									CONS.Admin.list_MainActv);
+		
+		// Set list to the view
+		this.setListAdapter(CONS.Main.mainAdapter);
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -21,11 +55,6 @@ public class MainActv extends Activity {
 		return true;
 	}
 
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-	}
 
 	@Override
 	protected void onDestroy() {
--- src/experiments/utils/CONS.java
+++ src/experiments/utils/CONS.java
@@ -1,11 +1,25 @@
 package experiments.utils;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Vibrator;
 import android.provider.MediaStore;
+import android.widget.ArrayAdapter;
 
 public class CONS {
 
-
+	public static class Admin {
+		
+		public static Vibrator vib;
+		
+		public static ArrayList<String> list_MainActv;
+	}
+	
+	public static class Main {
+		
+		public static ArrayAdapter mainAdapter;
+		
+	}
 }
